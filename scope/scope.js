db7dc8d8840284760b53c2cb51f53a7d2c8744d1
AhinavKalluri <Kalluriabhinav972@gmail.com>
2025-04-27 05:33:15 +0000

    scope

---
+++ scope/scope.js
@@ -0,0 +1,9 @@
+if(true){
+    const a=10//by this we can see that const is used within the block
+    let b=20//by this we can see that let is used within the block
+    var c=30//var is global scopee
+}
+
+// console.log(a)
+// console.log(b)
+console.log(c)
