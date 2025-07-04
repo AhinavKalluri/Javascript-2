871d3f53eec842ee1ccac3088aabd0114e801119
AhinavKalluri <Kalluriabhinav972@gmail.com>
2025-07-04 14:08:48 +0000

    after-long

--- projects/proj3/script.js
+++ projects/proj3/script.js
@@ -2,4 +2,4 @@ const clock = document.querySelector('.clock');
 
 setInterval(() => {
 let time = new Date();
-clock.innerHTML = time.toLocaleTimeString();}, 1000);
+clock.innerHTML = time.toLocaleTimeString();}, 1000)
--- projects/proj4/proj5(unlimitcolor)/script.js
+++ projects/proj4/proj5(unlimitcolor)/script.js
@@ -1,11 +1,29 @@
-// generating a random color
-
-const randomColor = function(){
-    const hex = '0123456789ABCDEF'
-    let color = '#'
-    for(let i=0;i<6;i++){
-        console.log(color += hex[Math.floor(Math.random)*16])
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
     }
+    console.log(color); // optional
+    return color;
+};
 
+let intervalID;
 
-}
+const starChangingColor = function () {
+    // Prevent multiple intervals
+        intervalID = setInterval(changeBGColor, 5  00);
+    
+
+    function changeBGColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+
+const stopChangingColor = function () {
+    clearInterval(intervalID);
+    intervalID=null;
+};
+
+document.querySelector("#start").addEventListener('click', starChangingColor);
+document.querySelector("#stop").addEventListener('click', stopChangingColor);
